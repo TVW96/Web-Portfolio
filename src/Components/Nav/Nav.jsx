eaed8e08cb52f56b17f5d8c12aef48c08daba379
Troy Washington <troyvw96@gmail.com>
2024-08-01 17:59:44 -0700

    bubble

--- src/Components/Nav/Nav.jsx
+++ src/Components/Nav/Nav.jsx
@@ -5,25 +5,25 @@ import ScrollToHashElement from "../../Utils/ScrollToHashElement";
 import "./Nav.scss";
 
 function Nav() {
-	return (
-		<nav className="navigation">
-			<ScrollToHashElement />
-			<div className="container">
-				<div className="nav-conatiner">
-					<div className="section_one">
-						<ul className="nav-item">
-							<img src={Logo} alt="logo" className="Logo" />
-						</ul>
-						<Link to="#Skills" className="nav-item">Skills</Link>
-						<Link to="#Experience" className="nav-item">Experience</Link>
-					</div>
-					<div className="section_two">
-						<Link to="#contact" className="nav-item">contact</Link>
-					</div>
+  return (
+    <nav className="navigation">
+      <ScrollToHashElement />
+      <div className="container">
+        <div className="nav-container">
+          <div className="section_one">
+            <ul className="nav-item logo-item">
+              <img src={Logo} alt="logo" className="Logo" />
+            </ul>
+            <Link to="#Skills" className="nav-item bubble-item">Skills</Link>
+            <Link to="#Experience" className="nav-item bubble-item">Experience</Link>
+          </div>
+          <div className="section_two">
+            <Link to="#contact" className="nav-item bubble-item">contact</Link>
+          </div>
         </div>
-			</div>
-		</nav>
-	);
+      </div>
+    </nav>
+  );
 }
 
 export default Nav;
